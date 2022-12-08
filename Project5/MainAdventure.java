fd516584c86ced7f31259eb4602b78602f7bb9b2
Tmeyer9045 <tmeyer9045@gmail.com>
2022-12-07 19:28:01 -0500

    Version 1- adding text
    This is the basic intro stage of the choose your own adventure project. All i'm doing right now is adding text so I have a basic outline of the whole adventure. Then I will add good/evil values, and add stats to things done/found while on the adventure.

---
+++ Project5/MainAdventure.java
@@ -0,0 +1,24 @@
+
+/**
+ * Write a description of class MainAdventure here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class MainAdventure extends Character
+{
+public static void main (String[] args) {
+    String newLine = System.lineSeparator(); 
+   System.out.println("After a long day on the UMW campus, you decided to get a nice coffee from Katora." + newLine + 
+                       "You were feeling adventurous and ordered the new menu item called the “nirvana." + newLine +
+                       "While waiting in line you noticed some promotional material for this drink, the black lettering seemed to jump out at you, the material read “transport yourself to a new world.” " + newLine +
+                       "What is your response to this advertising?" ); 
+   System.out.println("");
+   System.out.println("1) The advertisement has done its job, and now you can’t wait to try this caffeine treat." + newLine +
+                      "2) The black letters felt, “inky”, in your mind… something isn’t quite right." + newLine +
+                      "3) You are quite sleepy from studying maybe the weird letters were a trick of the mind, all" + newLine +
+                         "the coffee will do is make sure nothing like that happens again" + newLine + 
+                      "4) After thinking it over, you aren’t thirsty enough or sleepy enough to want coffee, and you leave" );
+}
+    
+}
---
+++ Project5/Reminder.java
@@ -0,0 +1,11 @@
+
+/**
+ * Write a description of class Reminder here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Reminder
+{
+   //Make a text file that is the summation of the adventure. 
+}
